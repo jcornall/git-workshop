c31aff7ec9163511f97c87eaa0031d2e4faface3
Saul Beck <saul.beck@stfc.ac.uk>
2025-08-11 14:56:20 +0100

    No flicker

--- smiley.sh
+++ smiley.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
-# Animated smiley face in terminal
+# Animated smiley face in terminal (Improved Version)
+
+# Frames for animation
 frames=(
-"  😊  "
-"  😐  "
-"  😉  "
-"  😄  "
-"  🐱‍👤 "
+    "  😊  "
+    "  😐  "
+    "  😉  "
+    "  😄  "
+    "  🤩  "
+    "  😎  "
 )
+
+# Animation speed (seconds)
+delay=0.3
+
+# Trap Ctrl+C to exit gracefully
+trap "tput cnorm; tput sgr0; clear; exit" SIGINT
+
+# Hide cursor
+tput civis
 
 while true; do
     for frame in "${frames[@]}"; do
-        clear
-        echo -e "\n\n\n\t\t$frame"
-        sleep 0.5
+        # Get terminal size
+        rows=$(tput lines)
+        cols=$(tput cols)
+
+        # Calculate center position
+        row=$((rows / 2))
+        col=$(( (cols - ${#frame}) / 2 ))
+
+        # Move cursor and print frame
+        tput cup "$row" "$col"
+        echo -ne "$frame"
+
+        sleep "$delay"
     done
 done
